092a71969caef8e8ef23e7c0b5ceb7abe11086ba
Alex Frd <chugun777akk@mail.ru>
2026-06-25 16:29:04 +0700

    stage 2 is done, add frontend to auth_base.html and login.html

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
+from apps.accounts.forms import UserLoginForm
 
 
 # Create your views here.
 class UserLoginView(LoginView):
+    form_class = UserLoginForm
     template_name = "accounts/login.html"
+    redirect_authenticated_user = True
 
 
 class UserLogoutView(LogoutView):
